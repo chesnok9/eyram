6b880a4e578a6d2b0263519419d8ee0419823940
chesnok9 <pavel.chesnovsky@gmail.com>
2024-03-04 23:41:34 +0100

    clear code

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import {
     Admin,
-    CustomRoutes,
     Resource,
     localStorageStore,
-    useStore,
     StoreContextProvider,
 } from 'react-admin';
 import { Route } from 'react-router';
 
 import authProvider from './authProvider';
-import categories from './categories';
 import { Dashboard } from './dashboard';
 import dataProviderFactory from './dataProvider';
 import englishMessages from './i18n/en';
-import invoices from './invoices';
 import { Layout, Login } from './layout';
-import orders from './orders';
-import products from './products';
-import reviews from './reviews';
-import Segments from './segments/Segments';
-import visitors from './visitors';
-import { themes, ThemeName } from './themes/themes';
 import { mainDarkTheme, mainLightTheme } from './themes/mainTheme';
 
 const i18nProvider = polyglotI18nProvider(
@@ -60,19 +50,7 @@ const App = () => {
             darkTheme={mainDarkTheme}
             defaultTheme="light"
         >
-            <CustomRoutes>
-                <Route path="/segments" element={<Segments />} />
-            </CustomRoutes>
-            <Resource name="customers" {...visitors} />
-            <Resource
-                name="commands"
-                {...orders}
-                options={{ label: 'Orders' }}
-            />
-            <Resource name="invoices" {...invoices} />
-            <Resource name="products" {...products} />
-            <Resource name="categories" {...categories} />
-            <Resource name="reviews" {...reviews} />
+            <Resource name="customers" />
         </Admin>
     );
 };
